6cf0d54bad60e975e4350378f1370ccf6819d2e2
SadipBhattarai <sadipbhattarai3@gmail.com>
2025-03-18 22:38:28 +0545

    Added user module with API testing route

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
+const userRouter = require("../modules/users/user.route");
 
+router.get("/", (req, res, next) => {
+    try{
+        res.json({data: "API is working Properly"});
+    } catch(e) {
+        next(e)
+    }
+});
 module.exports = router;
